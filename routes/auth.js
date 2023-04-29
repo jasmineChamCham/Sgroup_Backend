ddd3898a538e7920a798583e5a3a2edf9a92ee97
jasmineChamCham <jasminebkdn@gmail.com>
2023-04-29 10:02:30 +0700

    validate in register and update flow

--- routes/auth.js
+++ routes/auth.js
@@ -6,22 +6,46 @@ const { hashPw, hashPwWithGivenSalt } = require('../helpers/hash')
 const authRouter = express.Router()
 const SECRET = process.env.SECRET
 
-function validateUser(username, password, next) {
-    const regexSpecial = /^[a-zA-Z0-9]+$/;
-    // const regexNumber = /^\d+(\.\d+)?$/;
+app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-    if (regexSpecial.test(username) && regexSpecial.test(password)) {
-        next();
-    } else {
-        return;
-    }
-}
-
-authRouter.post('/register', (req, res) => {
+function validateRegisterRequest(req, res, next) {
     const { username, password, confirm_password, name, age, gender, email } = req.body
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const unsignedNumberRegex = /^\d+$/
     if (password !== confirm_password) {
         return res.json({ message: 'password and confirm_password dont match' })
     }
+
+    if (username.length < 3) {
+        return res.json({ message: 'username str length must be greater or equal to 3' })
+    }
+
+    if (password.length < 3) {
+        return res.json({ message: 'password str length must be greater or equal to 3' })
+    }
+
+    if (!emailRegex.test(email)) {
+        return res.json({ message: 'wrong email' })
+    }
+
+    if (name.length < 2) {
+        return res.json({ message: 'name str length must be greater or equal to 2' })
+    }
+
+    if (!gender) {
+        return res.json({ message: 'no input for gender' })
+    }
+
+    if (!unsignedNumberRegex.test(age)) {
+        return res.json({ message: 'age must me unsigned number' })
+    }
+    next()
+}
+
+authRouter.post('/register', validateRegisterRequest, (req, res) => {
+    const { username, password, confirm_password, name, age, gender, email } = req.body
 
     db.query('select * from users where username=?', username, (err, rs) => {
         if (err) {
@@ -106,12 +130,11 @@ function verifyToken(req, res, next) {
 
     jsonwebtoken.verify(token, SECRET, (err, user) => {
         if (err) {
-            return res.status(403).json({ error: err });
+            return res.status(403).json({ eror: err });
         }
-        req.user = user;
-        console.log("verify token user: ", user)
 
         if (user.id == req.params.id) {
+            console.log("verify token user: ", user)
             next();
         } else {
             console.log("user id = ", user.id)
@@ -121,22 +144,39 @@ function verifyToken(req, res, next) {
     });
 }
 
-// Define a route to handle user updates
-authRouter.put('/users/:id', verifyToken, async (req, res) => {
+function validateUpdateRequest(req, res, next) {
+    const { password, name, age, gender } = req.body;
+
+    if (!name) {
+        console.log("name: " + name)
+        return res.status(400).json({ error: 'Missing name field' });
+    }
+    if (!age) {
+        console.log("age: " + age)
+        return res.status(400).json({ error: 'Missing age field' });
+    }
+
+    const unsignedNumberRegex = /^\d+$/
+
+    if (!unsignedNumberRegex.test(age)) {
+        return res.json({ message: 'age must be unsigned number' })
+    }
+
+    next()
+
+}
+
+authRouter.put('/users/:id', verifyToken, validateUpdateRequest, async (req, res) => {
     try {
         const { id } = req.params;
         const { password, name, age, gender } = req.body;
 
-        // Validate request
-        if (!name || !age || !gender) {
-            return res.status(400).json({ error: 'Missing required fields' });
-        }
         let user;
 
         await db.query('select * from users where id = ?', id, (err, rs) => {
             if (err) {
                 return res.status(500).json({
-                    message: ' Internal Server Error'
+                    message: "hehe " + err
                 })
             }
 
@@ -155,7 +195,7 @@ authRouter.put('/users/:id', verifyToken, async (req, res) => {
         db.query('update users set name=?, age=?, gender=? where id = ?', [name, age, gender, id]
             , (err) => {
                 if (err) {
-                    return res.status(500).json({ message: ' Internal Server Error' })
+                    return res.status(500).json({ message: ' hihi ' + err })
                 }
 
                 const jwt = jsonwebtoken.sign({
